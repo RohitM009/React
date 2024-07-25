172c2e06623b878a6319e1f8750aaa3324279d3a
rohit <maske0901@gmail.com>
2024-07-25 08:08:46 +0530

    todo-app-localstorage

---
+++ 10_todo_contextAPI_local/src/contexts/TodoContext.js
@@ -0,0 +1,26 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:"TODO MSG",
+            completed:false
+        }
+    ],
+    addTodo : (todo)=>{},
+    updateTodo : (id,todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleComplete : (id)=>{},
+    // clearCompleted : ()=>{},
+    // filter : ()=>{},
+    // filterType : "all"
+
+})
+
+
+export const useTodo =()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider=TodoContext.Provider
