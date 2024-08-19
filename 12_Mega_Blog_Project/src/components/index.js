de902689eb971bac7d8fd186aad846b45fa34281
rohit <maske0901@gmail.com>
2024-08-19 19:53:24 +0530

    Pages-building

--- 12_Mega_Blog_Project/src/components/index.js
+++ 12_Mega_Blog_Project/src/components/index.js
@@ -5,10 +5,13 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import Button from './Button'
-import Input from './Input'
 import RTE from './RTE'
-
+import Signup from './SignUp'
+import Login from './Login'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from "./AuthLayout"
 export{
-    Header,LogoutBtn,
-    Footer,Container,Logo,Button,Input,RTE
+    Header,LogoutBtn,Signup,Login,PostCard,
+    Footer,Container,Logo,Button,RTE,PostForm,AuthLayout
 }
--- 12_Mega_Blog_Project/src/main.jsx
+++ 12_Mega_Blog_Project/src/main.jsx
@@ -1,13 +1,77 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { createBrowserRouter } from "react-router-dom";
+import { AuthLayout, Login } from "./components/index.js";
+import SignUp from "./components/SignUp.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "./login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignUp />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <Addpost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        post: "./post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
